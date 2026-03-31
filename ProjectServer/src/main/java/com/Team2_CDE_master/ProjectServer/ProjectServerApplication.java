92a7aa8b8ed21c3f5d6ba7090c1db8e099ce44ee
farahsharshar <Farah.hassan05@eng-st.cu.edu.eg>
2026-03-31 15:31:12 +0200

    setup: add Dockerfiles, docker-compose, and CI/CD workflow

---
+++ ProjectServer/src/main/java/com/Team2_CDE_master/ProjectServer/ProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.Team2_CDE_master.ProjectServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServerApplication.class, args);
+	}
+
+}
